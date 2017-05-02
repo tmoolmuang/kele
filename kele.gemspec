b582f473c106f14de0ea778bc1899ae2e1684705
Thanya Moolmuang <tmoolmuang@yahoo.com>
2017-05-02 16:19:08 +0000

    add user

--- kele.gemspec
+++ kele.gemspec
@@ -1,6 +1,3 @@
-# lib = File.expand_path("../lib", __FILE__)
-# $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
 Gem::Specification.new do |s|
   s.name          = 'kele'
   s.version       = '0.0.1'
@@ -15,4 +12,5 @@ Gem::Specification.new do |s|
     'http://rubygems.org/gems/kele'
   s.license       = 'MIT'
   s.add_runtime_dependency 'httparty', '~> 0.13'
+  s.add_runtime_dependency 'json'
 end
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,6 @@
 require "kele/error"
 require "httparty"
+require 'json'
 
 class Kele
   include HTTParty
@@ -9,6 +10,11 @@ class Kele
     response = self.class.post("/sessions", body: {"email": email, "password": password})
     raise KeleError.new(response.message) unless response.code == 200
     @auth_token = response["auth_token"]
+  end
+  
+  def get_me
+    response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
   end
   
 end
